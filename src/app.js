4b6ceb69a9e21cbc9be8ec1d6f23c8b1eea8e445
Luis <lalpizarbr@gmail.com>
2026-03-03 03:39:03 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let quien = [
+    "El gato",
+    "Mi abuela",
+    "El cartero",
+    "Mi pájaro"
+  ];
+
+  let como = [
+    "se comió",
+    "orinó",
+    "falló",
+    "rompió"
+  ];
+
+  let que = [
+    "el trabajo",
+    "el proyecto",
+    "el código",
+    "el archivo"
+  ];
+
+  let cuando = [
+    "justo antes de enviarlo.",
+    "anoche.",
+    "esta mañana.",
+    "5 minutos antes."
+  ];
+
+  document.getElementById("generate").addEventListener("click", function () {
+    let excuse =
+      quien[Math.floor(Math.random() * quien.length)] + " " +
+      como[Math.floor(Math.random() * como.length)] + " " +
+      que[Math.floor(Math.random() * que.length)] + " " +
+      cuando[Math.floor(Math.random() * cuando.length)];
+
+    document.getElementById("excusa").innerHTML = excuse;
+  });
 };
